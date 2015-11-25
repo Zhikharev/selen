1991c638b1c6685d2c34bca950b9f7ac98ccabcd
pointhex <1111artem1111@gmail.com>
2015-11-25 23:42:33 +0300

    IO hub
    Top level I/O was set.

--- rtl/io_hub/rtl/io_top.v
+++ rtl/io_hub/rtl/io_top.v
@@ -1,4 +1,24 @@
-
-module io_top();
+module io_top(
+		input clk,
+		input rst,
+		
+		//UART
+		input rx,
+		
+		//IO inteface data
+		input  				io_stb_i,
+		output 	 			io_ack_o,
+		input         		io_we_i,
+		input 	[15:0] 		io_addr_i,
+		output 	[31:0] 		io_data_o,
+		
+		// DMA
+		input 				dma_cyc_i,
+		output 				dma_stb_o,
+		input				dma_ack_i, 
+		output 				dma_we_o,
+		output	[15:0] 		dma_addr_o,
+		output 	[31:0]		dma_data_o
+);
 
 endmodule
