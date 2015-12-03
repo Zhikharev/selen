acb92a69f7a19c5aaa0b9e2f0bba1f158f309d78
Artem Sokolovscii <you@example.com>
2015-12-03 15:04:19 +0300

    Memory commutator
    Memory map
    ROM 0x0000
        0x000F
    RAM 0x4000
        0x7FFF
    IO  0x8000
        0xFFFF

--- rtl/memory_commutator/rtl/commutator.v
+++ rtl/memory_commutator/rtl/commutator.v
@@ -68,14 +68,26 @@ module commutator
 	output   	[15:0]		slave3_wb_addr_o,
 	input 	  	[31:0]		slave3_wb_data_i
 );
-	parameter M_ROM_h = 16'h0000;
-	parameter M_ROM_l = 16'h000C;
+	/*parameter M_ROM_h = 16'h0000;
+	parameter M_ROM_l = 16'h000F;
 
-	parameter M_RAM_h = 16'h0200;
+	parameter M_RAM_h = 16'h0010;
 	parameter M_RAM_l = 16'h03FF;
 
 	parameter M_IO_h = 16'h0400;
-	parameter M_IO_l = 16'hFFFF;
+	parameter M_IO_l = 16'hFFFF;*/
+	
+	//ROM
+	localparam SL3_ADDR = 16'h0000; 
+	localparam SL3_MASK = 16'hFFF0;
+	
+	//RAM
+	localparam SL2_ADDR = 16'h4000; 
+	localparam SL2_MASK = 16'hC000;
+	
+	//IO
+	localparam SL4_ADDR = 16'h8000; 
+	localparam SL4_MASK = 16'h8000;
 	
 	parameter DEPTH = 3;
 	parameter SIZE	= 16;
@@ -89,28 +101,32 @@ module commutator
 	reg			[31:0] 		m_data_o;
 	
 	//FIFO
-	wire full, full_n;
-	wire empty, empty_n;
+	wire full;
+	wire empty;
 	wire [15:0] fifo_master1_addr;
-	wire re;
-	wire [1:0] level;
 	
 	reg	 			reg_master1_wb_ack_o;
 	reg [31:0]		reg_master1_wb_data_o;
+	
 	reg	 			reg_master2_wb_ack_o; 
 	reg	[31:0] 		reg_master2_wb_data_o;
+	
 	reg   			reg_slave4_wb_stb_o;
 	reg        		reg_slave4_wb_we_o;
 	reg [15:0] 		reg_slave4_wb_addr_o;
 	reg [31:0] 		reg_slave4_wb_data_o;
+	
 	reg	 			reg_master3_wb_ack_o; 
 	reg	[31:0] 		reg_master3_wb_data_o;
+	
 	reg    			reg_slave1_wb_stb_o;
 	reg [15:0]		reg_slave1_wb_addr_o;
+	
 	reg	    		reg_slave2_wb_stb_o;
 	reg	 			reg_slave2_wb_we_o;
 	reg	[15:0]		reg_slave2_wb_addr_o;
 	reg	[31:0]		reg_slave2_wb_data_o;
+	
 	reg				reg_slave3_wb_stb_o;
 	reg [15:0]		reg_slave3_wb_addr_o;
 	
@@ -162,58 +178,59 @@ module commutator
 		.empty(empty),
 		.full(full)
 	);
-	assign re = empty_n;
 	
 	//overflow
 	assign master1_wb_stall_o = full;
 
 	// cpu_inst <=> ram or rom
 	always @(*) begin
-		if ((master1_wb_addr_i > M_ROM_l) && (master1_wb_addr_i < M_ROM_h)) begin
+		//if ((master1_wb_addr_i > M_ROM_l) && (master1_wb_addr_i < M_ROM_h)) begin
+		if (master1_wb_addr_i & SL3_MASK == SL3_ADDR) begin
 			//slave1_wb_stb_o 			<= master1_wb_stb_i;
 			reg_slave1_wb_stb_o 			<= ~empty;
 			reg_master1_wb_ack_o			<= slave1_wb_ack_i;
-			reg_slave1_wb_addr_o			<= fifo_master1_addr;
+			reg_slave1_wb_addr_o			<= fifo_master1_addr & (~SL3_MASK);
 			reg_master1_wb_data_o			<= slave1_wb_data_i;		
 		end else begin
 			//slave3_wb_stb_o 			<= master1_wb_stb_i;
 			reg_slave3_wb_stb_o 			<= ~empty;
 			reg_master1_wb_ack_o			<= slave3_wb_ack_i;
-			reg_slave3_wb_addr_o			<= fifo_master1_addr;
+			reg_slave3_wb_addr_o			<= fifo_master1_addr & (~SL2_MASK);
 			reg_master1_wb_data_o			<= slave3_wb_data_i;
 		end
 	end
 	
 	// cpu_data <=> ram or I/O
 	always @(*) begin
-		if ((master1_wb_addr_i > M_RAM_l) && (master1_wb_addr_i < M_RAM_h)) begin
+		//if ((master1_wb_addr_i > M_RAM_l) && (master1_wb_addr_i < M_RAM_h)) begin
+		if (master1_wb_addr_i & SL2_MASK == SL2_ADDR) begin
 			reg_slave2_wb_stb_o 			<= m_stb_i;
-			m_ack_o						<= slave2_wb_ack_i;
+			m_ack_o							<= slave2_wb_ack_i;
 			reg_slave2_wb_we_o				<= m_we_i;
-			reg_slave2_wb_addr_o			<= m_addr_i;
-			m_data_o					<= slave2_wb_data_i;		
+			reg_slave2_wb_addr_o			<= m_addr_i & (~SL2_MASK);
+			m_data_o						<= slave2_wb_data_i;		
 			reg_slave2_wb_data_o			<= m_data_i;
 			
-			if (master2_wb_stb_i) begin				
-				m_stb_i 				<= master2_wb_stb_i;
+			if (master2_wb_stb_i & master1_wb_cyc_i) begin				
+				m_stb_i 					<= master2_wb_stb_i;
 				reg_master2_wb_ack_o		<= m_ack_o;
-				m_we_i 					<= master2_wb_we_i;
-				m_addr_i				<= master2_wb_addr_i;
-				m_data_i				<= master2_wb_data_i;
+				m_we_i 						<= master2_wb_we_i;
+				m_addr_i					<= master2_wb_addr_i;
+				m_data_i					<= master2_wb_data_i;
 				reg_master2_wb_data_o		<= m_data_o;
 			end else begin
-				m_stb_i 				<= master3_wb_stb_i;
+				m_stb_i 					<= master3_wb_stb_i;
 				reg_master3_wb_ack_o		<= m_ack_o;
-				m_we_i 					<= master3_wb_we_i;
-				m_addr_i				<= master3_wb_addr_i;
-				m_data_i				<= master3_wb_data_i;
+				m_we_i 						<= master3_wb_we_i;
+				m_addr_i					<= master3_wb_addr_i;
+				m_data_i					<= master3_wb_data_i;
 				reg_master3_wb_data_o		<= m_data_o;
 			end		
 		end else begin
 			reg_slave4_wb_stb_o 			<= master2_wb_stb_i;
 			reg_master2_wb_ack_o			<= slave4_wb_ack_i;
 			reg_slave4_wb_we_o				<= master2_wb_we_i;
-			reg_slave4_wb_addr_o			<= master2_wb_addr_i;
+			reg_slave4_wb_addr_o			<= master2_wb_addr_i & (~SL4_MASK);
 			reg_master2_wb_data_o			<= slave4_wb_data_i;
 			reg_slave4_wb_data_o			<= master2_wb_data_i;
 		end
