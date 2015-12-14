401ab0fe862bea397a7312216f97cd0dbccc9a16
AlexBolotnikov <blcorporation@mail.ru>
2015-12-14 17:31:35 +0300

    verification

--- verif/cpu/sequences/cpu_seq_lib.sv
+++ verif/cpu/sequences/cpu_seq_lib.sv
@@ -57,4 +57,116 @@ class cpu_load_seq extends base_seq;
 
 endclass
 
+class cpu_test_seq extends base_seq;
+	
+	function new();
+		super.new();
+	endfunction
+	
+	
+	task body();
+		$display("[%0t][SEQ] Start of 'cpu_test_seq'", $time);
+		req = new();
+		assert(req.randomize())
+		else $fatal("Randomization failed!");
+		req.opcode = ADDI;
+		req.rd = 1;
+		req.rs1 = 0;
+		req.imm = 20'b1;
+		req = new();
+		assert(req.randomize())
+		else $fatal("Randomization failed!");
+		req_q.push_back(req);
+		req.opcode = ADDI;
+		req.rd = 2;
+		req.rs1 = 0;
+		req.imm = 20'b1;
+		req_q.push_back(req);
+		req = new();
+		assert(req.randomize())
+		else $fatal("Randomization failed!");
+		req.opcode = ADD;
+		req.rd = 3;
+		req.rs1 = 1;
+		req.rs2 = 2;
+		req_q.push_back(req);
+	endtask
+
+endclass
+
+class cpu_bp_seq extends base_seq;
+
+	function new();
+		super.new();
+	endfunction
+	
+	task body();
+		$display("[%0t][SEQ] Start of 'cpu_test_seq'", $time);
+		req = new();
+		assert(req.randomize())
+		else $fatal("Randomization failed!");
+		req.opcode = ADD;
+		req.rd = 10;
+		req.rs1 = 1;
+		req.rs2 = 2;
+		req_q.push_back(req);
+		req = new();
+		assert(req.randomize())
+		else $fatal("Randomization failed!");
+		req.opcode = ADD;
+		req.rd = 3;
+		req.rs1 = 10;
+		req.rs2 = 2;
+		req_q.push_back(req);
+		req = new();
+		assert(req.randomize())
+		else $fatal("Randomization failed!");
+		req.opcode = ADD;
+		req.rd = 5;
+		req.rs1 = 1;
+		req.rs2 = 3;
+		req_q.push_back(req);
+		req = new();
+		assert(req.randomize())
+		else $fatal("Randomization failed!");
+		req.opcode = ADD;
+		req.rd = 0;
+		req.rs1 = 0;
+		req.rs2 = 0;
+		req_q.push_back(req);
+		req = new();
+		assert(req.randomize())
+		else $fatal("Randomization failed!");
+		req.opcode = ADD;
+		req.rd = 7;
+		req.rs1 = 5;
+		req.rs2 = 2;
+		req_q.push_back(req);
+	endtask
+endclass
+
+
+class cpu_ADDI_seq extends base_seq;
+
+  function new ();
+    super.new();
+  endfunction 
+	int i=0;
+  task body();
+    $display("[%0t][SEQ] Start of 'cpu_load_seq'", $time);
+    repeat(32) begin
+      req = new();
+      assert(req.randomize())
+      else $fatal("Randomization failed!");
+      req.opcode = ADDI;
+      req.rd = i;
+      req.rs1 = 0;
+      req.imm = i;	
+      i = i +1;
+      req_q.push_back(req);
+    end
+    $display ("[%0t][SEQ] End of 'cpu_load_seq'", $time);  
+  endtask 
+
+endclass
 `endif
