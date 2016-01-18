c692fdba9e5d7fb2216d3bf0b97cb4d2db1eaf37
Jettatura <jettatura@kinetix.ru>
2016-01-18 22:24:43 +0300

    rework bare metal example

--- sim/bare_metal/run.sh
+++ sim/bare_metal/run.sh
@@ -3,9 +3,10 @@
 IMAGE_SIZE=4096
 IMAGE_NAME="flash.bin"
 
-/home/jettatura/bin/riscv/bin/riscv64-unknown-elf-gcc -m32 -nostdlib -nostartfiles -Ttext=0x0 -o temp.elf test.cpp
+/home/jettatura/bin/riscv/bin/riscv64-unknown-elf-as -m32 startup.s -g -o startup.o
+/home/jettatura/bin/riscv/bin/riscv64-unknown-elf-gcc -m32 -c -o temp.o test.cpp
 
-/home/jettatura/bin/riscv/bin/riscv64-unknown-elf-gcc -m32 -nostdlib -nostartfiles -S -o temp.s test.cpp
+/home/jettatura/bin/riscv/bin/riscv64-unknown-elf-ld -melf32lriscv -T linker.ld startup.o temp.o -o temp.elf
 
 /home/jettatura/bin/riscv/bin/riscv64-unknown-elf-objcopy -O binary -S --set-start 0 temp.elf temp.bin
 
@@ -13,4 +14,3 @@ dd if=temp.bin of=$IMAGE_NAME bs=$IMAGE_SIZE conv=notrunc
 
 #Запускать в симуляторе
 #./sim -img  ../src/sim/bare_metal/flash.bin -i -e BE
-
---
+++ sim/bare_metal/startup.s
@@ -0,0 +1,5 @@
+.global _entry
+_entry:
+ la sp, stack_top
+ j main
+ 
