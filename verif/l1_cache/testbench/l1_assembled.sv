fa198beee6486c71683a662afe50c2a9aa2d5f44
MaksimKobzar <maksim.s.kobzar@gmail.com>
2016-02-07 19:51:34 +0300

    [l1][verif] add wb_if, tb_top and l1_assembled

---
+++ verif/l1_cache/testbench/l1_assembled.sv
@@ -0,0 +1,55 @@
+`ifndef INC_L1_ASSEMBLED
+`define INC_L1_ASSEMBLED
+
+module l1_assembled
+(
+	input 	clk,
+	input 	rst_n,
+  core_if l1i_intf,
+  core_if l1d_intf
+	wb_if 	wb_intf
+);
+
+l1_dut l1
+(
+	.clk                    (clk),
+  .rst_n                  (rst_n),
+
+  // L1I interface
+  .l1i_req_val            (l1i_intf.req_val),
+  .l1i_req_addr           (l1i_intf.req_addr),
+  .l1i_req_ack            (l1i_intf.req_ack),
+  .l1i_ack_data           (l1i_intf.ack_data),
+
+  // L1D interface
+  .l1d_req_val            (l1d_intf.req_val),
+  .l1d_req_addr           (l1d_intf.req_addr),
+  .l1d_req_cop            (l1d_intf.req_cop),
+  .l1d_req_wdata          (l1d_intf.req_wdata),
+  .l1d_req_size           (l1d_intf.req_size),
+  .l1d_req_be             (l1d_intf.req_be),
+  .l1d_req_ack            (l1d_intf.req_ack),
+  .l1d_ack_data           (l1d_intf.ack_data),
+
+  // Wishbone B4 interface
+  .wb_clk_i               (wb_intf.clk_i),
+  .wb_rst_i               (wb_intf.rst_i),
+  .wb_dat_i               (wb_intf.dat_i),
+  .wb_dat_o               (wb_intf.dat_o),
+  .wb_ack_i               (wb_intf.ack_i),
+  .wb_adr_o               (wb_intf.adr_o),
+  .wb_cyc_o               (wb_intf.cyc_o),
+  .wb_stall_i             (wb_intf.stall_i),
+  .wb_err_i               (wb_intf.err_i),    // not used now
+  .wb_lock_o              (wb_intf.lock_o),   // not used now
+  .wb_rty_i               (wb_intf.rty_i),    // not used now
+  .wb_sel_o               (wb_intf.sel_o),
+  .wb_stb_o               (wb_intf.stb_o),
+  .wb_tga_o               (wb_intf.tga_o),    // not used now
+  .wb_tgc_o               (wb_intf.tgc_o),    // not used now
+  .wb_we_o                (wb_intf.we_o)
+);
+
+endmodule
+
+`endif
---
+++ verif/l1_cache/testbench/l1_tb_top.sv
@@ -0,0 +1,65 @@
+// ----------------------------------------------------------------------------
+//
+// ----------------------------------------------------------------------------
+// FILE NAME      : l1_tb_top.sv
+// PROJECT        : Selen
+// AUTHOR         : Maksim Kobzar
+// AUTHOR'S EMAIL :
+// ----------------------------------------------------------------------------
+// DESCRIPTION    :
+// ----------------------------------------------------------------------------
+
+`ifndef INC_L1_TB_TOP
+`define INC_L1_TB_TOP
+
+module l1_tb_top;
+
+	reg sys_clk;
+	wire reset;
+
+  initial begin
+    sys_clk = 1'b0;
+    `uvm_info("DBG", $sformatf("sys_clk half period = %0d", `CLK_HALF_PERIOD), UVM_NONE)
+    forever #(`CLK_HALF_PERIOD) sys_clk = ~sys_clk;
+  end
+
+  // Reset interface
+  rst_if rst_intf(sys_clk);
+  assign reset = rst_intf.rst | rst_intf.soft_rst;
+
+  wb_if    	wb_intf(sys_clk, reset);
+  core_if   l1i_intf(sys_clk, reset);
+  core_if   lid_intf(sys_clk, reset);
+
+  l1_assembled dut
+  (
+  	.clk 			    (sys_clk),
+  	.rst_n   	    (!reset),
+  	.wb_intf      (wb_intf),
+    .l1i_intf    	(l1i_intf),
+    .lid_intf   	(lid_intf)
+  );
+
+  typedef virtual core_if   v_core;
+  typedef virtual wb_if   	v_wb;
+
+  initial begin
+    uvm_config_db#(virtual rst_if)::set(null,  "*rst_agent*", "vif", rst_intf)	;
+    uvm_config_db#(core_if)::set(uvm_root::get(),"*l1i_agent*", "vif", l1i_intf);
+    uvm_config_db#(core_if)::set(uvm_root::get(),"*lid_agent*", "vif", l1d_intf);
+    uvm_config_db#(v_wb)::set(uvm_root::get(),		"*wb_agent*", "vif", wbintf)	;
+  end
+
+  bit [31:0] seed;
+
+    initial begin
+      seed = $get_initial_random_seed();
+      `uvm_info("DBG", $sformatf("SEED = %0d", seed), UVM_NONE)
+      #0;
+      run_test();
+    end
+
+  initial $timeformat(-9, 1, "ns", 4);
+endmodule
+
+`endif
