5f951729f53aca5ff1efbc645370407693d40b99
Grigoriy Zhikharev <zhiharev@tecon.ru>
2016-02-20 05:31:20 +0300

    [verif] Added scripts fot cache verif

---
+++ verif/l1_cache/uvm/uvm.inc.sv
@@ -0,0 +1,13 @@
+`ifndef UVM_INC
+`define UVM_INC
+
+`include "uvm_pkg.sv"
+`include "uvm_macros.svh"
+
+import uvm_pkg::*;
+
+// Common
+`include "smart_report_server.sv"
+`include "core/sl_core_inc.sv"
+
+`endif
