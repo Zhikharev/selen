c6c1606f671ae7b5e7eeddde7e2da06c518e645a
Grigoriy Zhikharev <zhiharev@tecon.ru>
2016-03-04 09:01:23 +0300

    Updated memory map

--- rtl/wb_com/wb_com_defines.v
+++ rtl/wb_com/wb_com_defines.v
@@ -27,8 +27,8 @@
 `define WB_COM_AWIDTH            32
 `define WB_COM_DWIDTH            32
 `define WB_COM_F_ASIZE           2
-`define WB_COM_S0_ABASE          32'h00000000
-`define WB_COM_S0_AMASK          16'h02fc
-`define WB_COM_S1_ABASE          32'h00000300
-`define WB_COM_S1_AMASK          2'b11
+`define WB_COM_S0_ABASE          32'h0000_0000
+`define WB_COM_S0_AMASK          16'h2fff
+`define WB_COM_S1_ABASE          32'h0001_0000
+`define WB_COM_S1_AMASK          32'h0004_ffff
 `endif
