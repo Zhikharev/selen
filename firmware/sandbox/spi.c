d62eea8da5dba194d8f4979014d7a8f847e8e180
Jettatura <jettatura@kinetix.ru>
2016-06-16 01:00:00 +0300

    firmware: fix spi.c

--- firmware/sandbox/spi.c
+++ firmware/sandbox/spi.c
@@ -1,51 +1,5 @@
 #include "spi.h"
 
-static inline
-/*avoid collision  with built-in name */
-void memcpy_(volatile void *dest, volatile void *src, size_t n)
-{
-   volatile char *csrc = (volatile char *)src;
-   volatile char *cdest = (volatile char *)dest;
-
-   for (int i=0; i<n; i++)
-       cdest[i] = csrc[i];
-}
-
-/**
- * read data from flash
- *
- * @param destination - buffer to store received data,
- * @param start_address - start 3-byte address at flash memory
- * @param size - size to read, must be a multiple of 3*4
- */
-void flash_read(volatile void *destination,
-                uint32_t start_address,
-                const size_t size)
-{
-    volatile SPI* spi = spi_init();
-    /*16 byte transaction*/
-    spi->CTRL = deposit(spi->CTRL, 0, 7, 128);
-
-    spi->CTRL |= CTR_LSB;
-
-    /*size that received at each transaction*/
-    const size_t TRANSACTION_RECEIVED_SIZE = 3*4;
-
-    /*all received data size*/
-    size_t received = 0;
-    while(received < size) {
-
-        uint32_t address = start_address + received;
-        spi_read(spi, address);
-
-        /*copy DATA[0], [1], [2] to destination buffer*/
-        memcpy_(destination + received, &spi->DATA[0], TRANSACTION_RECEIVED_SIZE);
-
-        /*Received size*/
-        received += TRANSACTION_RECEIVED_SIZE;
-    }
-}
-
 uint32_t flash_read_word(uint32_t address)
 {
     volatile SPI* spi = spi_init();
@@ -60,13 +14,19 @@ uint32_t flash_read_word(uint32_t address)
 
 int __attribute__((optimize("Os"))) main()
 {
-    uint32_t size = flash_read_word(0);
+    const uint32_t size = flash_read_word(0);
 
     /*RAM*/
-    volatile uint8_t* ram = (uint8_t*)RAM_BASE_ADDRESS;
+    volatile uint32_t* ram = (uint32_t*)RAM_BASE_ADDRESS;
 
-    /*  read size bytes from 0x4*/
-    flash_read(ram, 0x4, size);
+    /*  read size bytes from DATA_OFFSET*/
+    size_t DATA_OFFSET = 0x4;
+    size_t received = 0;
+    while(received < size)
+    {
+        *(ram++) = flash_read_word(DATA_OFFSET + received);
+        received += 4;
+    }
 
     /*jump and start execution there*/
     jump_to(ram);
--- firmware/sandbox/spi.s
+++ firmware/sandbox/spi.s
@@ -53,38 +53,38 @@
 	.globl	spi_transaction
 	.type	spi_transaction, @function
 spi_transaction:
-	lw	a5,24(a0)	# D.1625, spi_3(D)->SS
-	or	a5,a5,1	# D.1625, D.1625,
-	sw	a5,24(a0)	# D.1625, spi_3(D)->SS
-	lw	a5,16(a0)	# D.1625, spi_3(D)->CTRL
-	or	a5,a5,256	# D.1625, D.1625,
-	sw	a5,16(a0)	# D.1625, spi_3(D)->CTRL
+	lw	a5,24(a0)	# D.1580, spi_3(D)->SS
+	or	a5,a5,1	# D.1580, D.1580,
+	sw	a5,24(a0)	# D.1580, spi_3(D)->SS
+	lw	a5,16(a0)	# D.1580, spi_3(D)->CTRL
+	or	a5,a5,256	# D.1580, D.1580,
+	sw	a5,16(a0)	# D.1580, spi_3(D)->CTRL
 	j	.L6	#
 .L8:
-	lw	a5,24(a0)	# D.1625, spi_3(D)->SS
-	and	a5,a5,-2	# D.1625, D.1625,
-	sw	a5,24(a0)	# D.1625, spi_3(D)->SS
+	lw	a5,24(a0)	# D.1580, spi_3(D)->SS
+	and	a5,a5,-2	# D.1580, D.1580,
+	sw	a5,24(a0)	# D.1580, spi_3(D)->SS
 .L6:
-	lw	a5,16(a0)	# D.1625,
-	and	a5,a5,256	# D.1625, D.1625,
-	bnez	a5,.L8	#, D.1625,
+	lw	a5,16(a0)	# D.1580,
+	and	a5,a5,256	# D.1580, D.1580,
+	bnez	a5,.L8	#, D.1580,
 	ret
 	.size	spi_transaction, .-spi_transaction
 	.align	2
 	.globl	spi_read
 	.type	spi_read, @function
 spi_read:
-	lw	a4,16(a0)	# D.1628, spi_5(D)->CTRL
-	lw	a3,16(a0)	# D.1628, spi_5(D)->CTRL
+	lw	a4,16(a0)	# D.1583, spi_5(D)->CTRL
+	lw	a3,16(a0)	# D.1583, spi_5(D)->CTRL
 	li	a5,16777216	# tmp94,
 	add	a5,a5,-1	# tmp93, tmp94,
-	sll	a2,a3,20	# tmp106, D.1628,
+	sll	a2,a3,20	# tmp106, D.1583,
 	and	a1,a1,a5	# address, address, tmp93
 	li	a5,0	# index,
 	bltz	a2,.L10	#, tmp106,
-	and	a5,a4,63	# D.1629, D.1628,
+	and	a5,a4,63	# D.1584, D.1583,
 	li	a3,32	# tmp98,
-	bgtu	a5,a3,.L22	#, D.1629, tmp98,
+	bgtu	a5,a3,.L22	#, D.1584, tmp98,
 .L11:
  #APP
 # 93 "spi.h" 1
@@ -93,91 +93,35 @@ spi_read:
 # 0 "" 2
  #NO_APP
 .L12:
-	srl	a5,a5,5	# D.1628, D.1629,
-	add	a5,a5,-1	# index, D.1628,
+	srl	a5,a5,5	# D.1583, D.1584,
+	add	a5,a5,-1	# index, D.1583,
 .L10:
 	sll	a5,a5,2	# tmp102, index,
 	li	a4,50331648	# tmp101,
 	add	a5,a0,a5	# tmp103, spi, tmp102
-	or	a1,a1,a4	# D.1628, address, tmp101
-	sw	a1,0(a5)	# D.1628, spi_5(D)->DATA
-	lw	a5,24(a0)	# D.1628, spi_5(D)->SS
-	or	a5,a5,1	# D.1628, D.1628,
-	sw	a5,24(a0)	# D.1628, spi_5(D)->SS
-	lw	a5,16(a0)	# D.1628, spi_5(D)->CTRL
-	or	a5,a5,256	# D.1628, D.1628,
-	sw	a5,16(a0)	# D.1628, spi_5(D)->CTRL
+	or	a1,a1,a4	# D.1583, address, tmp101
+	sw	a1,0(a5)	# D.1583, spi_5(D)->DATA
+	lw	a5,24(a0)	# D.1583, spi_5(D)->SS
+	or	a5,a5,1	# D.1583, D.1583,
+	sw	a5,24(a0)	# D.1583, spi_5(D)->SS
+	lw	a5,16(a0)	# D.1583, spi_5(D)->CTRL
+	or	a5,a5,256	# D.1583, D.1583,
+	sw	a5,16(a0)	# D.1583, spi_5(D)->CTRL
 	j	.L21	#
 .L23:
-	lw	a5,24(a0)	# D.1628, spi_5(D)->SS
-	and	a5,a5,-2	# D.1628, D.1628,
-	sw	a5,24(a0)	# D.1628, spi_5(D)->SS
+	lw	a5,24(a0)	# D.1583, spi_5(D)->SS
+	and	a5,a5,-2	# D.1583, D.1583,
+	sw	a5,24(a0)	# D.1583, spi_5(D)->SS
 .L21:
-	lw	a5,16(a0)	# D.1628,
-	and	a5,a5,256	# D.1628, D.1628,
-	bnez	a5,.L23	#, D.1628,
+	lw	a5,16(a0)	# D.1583,
+	and	a5,a5,256	# D.1583, D.1583,
+	bnez	a5,.L23	#, D.1583,
 	ret
 .L22:
-	and	a4,a4,31	# D.1628, D.1628,
-	beqz	a4,.L12	#, D.1628,
+	and	a4,a4,31	# D.1583, D.1583,
+	beqz	a4,.L12	#, D.1583,
 	j	.L11	#
 	.size	spi_read, .-spi_read
-	.align	2
-	.globl	flash_read
-	.type	flash_read, @function
-flash_read:
-	add	sp,sp,-32	#,,
-	sw	s1,20(sp)	#,
-	sw	s2,16(sp)	#,
-	sw	s3,12(sp)	#,
-	sw	ra,28(sp)	#,
-	sw	s0,24(sp)	#,
-	li	a3,4096	# tmp94,
-	mv	s2,a0	# destination, destination
-	mv	s3,a1	# start_address, start_address
-	mv	s1,a2	# size, size
-	sw	zero,16(a3)	#, MEM[(volatile struct SPI *)4096B].CTRL
-.L25:
-	lw	a5,16(a3)	# D.1645, MEM[(volatile struct SPI *)4096B].CTRL
-	li	a4,4096	# tmp95,
-	and	a5,a5,256	# D.1645, D.1645,
-	bnez	a5,.L25	#, D.1645,
-	li	a5,4	# tmp98,
-	sw	a5,20(a4)	# tmp98, MEM[(volatile struct SPI *)4096B].DIVIDER
-	lw	a3,16(a4)	# D.1645, MEM[(volatile struct SPI *)4096B].CTRL
-	add	a5,a4,-2048	# tmp104, tmp95,
-	li	s0,0	# received,
-	and	a3,a3,-128	# D.1647, D.1645,
-	sw	a3,16(a4)	# D.1647, MEM[(volatile struct SPI *)4096B].CTRL
-	lw	a3,16(a4)	# D.1645, MEM[(volatile struct SPI *)4096B].CTRL
-	or	a5,a3,a5	# D.1645, D.1645, tmp104
-	sw	a5,16(a4)	# D.1645, MEM[(volatile struct SPI *)4096B].CTRL
-	beqz	s1,.L24	#, size,
-.L30:
-	add	a1,s3,s0	#, start_address, received
-	li	a0,4096	#,
-	call	spi_read	#
-	li	a5,4096	# ivtmp.23,
-	add	a3,s2,s0	# ivtmp.24, destination, received
-	add	a2,a5,12	# D.1651, ivtmp.23,
-.L27:
-	lbu	a4,0(a5)	#, *_24
-	add	a3,a3,1	# ivtmp.24, ivtmp.24,
-	add	a5,a5,1	# ivtmp.23, ivtmp.23,
-	and	a4,a4,0xff	# D.1649, *_24
-	sb	a4,-1(a3)	# D.1649, *_23
-	bne	a5,a2,.L27	#, ivtmp.23, D.1651,
-	add	s0,s0,12	# received, received,
-	bgtu	s1,s0,.L30	#, size, received,
-.L24:
-	lw	ra,28(sp)	#,
-	lw	s0,24(sp)	#,
-	lw	s1,20(sp)	#,
-	lw	s2,16(sp)	#,
-	lw	s3,12(sp)	#,
-	add	sp,sp,32	#,,
-	jr	ra	#
-	.size	flash_read, .-flash_read
 	.align	2
 	.globl	flash_read_word
 	.type	flash_read_word, @function
@@ -188,25 +132,25 @@ flash_read_word:
 	li	a4,4096	# tmp83,
 	mv	a1,a0	# address, address
 	sw	zero,16(a4)	#, MEM[(volatile struct SPI *)4096B].CTRL
-.L36:
-	lw	a5,16(a4)	# D.1654, MEM[(volatile struct SPI *)4096B].CTRL
+.L25:
+	lw	a5,16(a4)	# D.1587, MEM[(volatile struct SPI *)4096B].CTRL
 	li	s0,4096	# tmp84,
-	and	a5,a5,256	# D.1654, D.1654,
-	bnez	a5,.L36	#, D.1654,
+	and	a5,a5,256	# D.1587, D.1587,
+	bnez	a5,.L25	#, D.1587,
 	li	a5,4	# tmp87,
 	sw	a5,20(s0)	# tmp87, MEM[(volatile struct SPI *)4096B].DIVIDER
-	lw	a5,16(s0)	# D.1654, MEM[(volatile struct SPI *)4096B].CTRL
+	lw	a5,16(s0)	# D.1587, MEM[(volatile struct SPI *)4096B].CTRL
 	add	a4,s0,-2048	# tmp93, tmp84,
 	mv	a0,s0	#, tmp84
-	and	a5,a5,-128	# D.1654, D.1654,
-	or	a5,a5,32	# D.1655, D.1654,
-	sw	a5,16(s0)	# D.1655, MEM[(volatile struct SPI *)4096B].CTRL
-	lw	a5,16(s0)	# D.1654, MEM[(volatile struct SPI *)4096B].CTRL
-	or	a5,a5,a4	# D.1654, D.1654, tmp93
-	sw	a5,16(s0)	# D.1654, MEM[(volatile struct SPI *)4096B].CTRL
+	and	a5,a5,-128	# D.1587, D.1587,
+	or	a5,a5,32	# D.1588, D.1587,
+	sw	a5,16(s0)	# D.1588, MEM[(volatile struct SPI *)4096B].CTRL
+	lw	a5,16(s0)	# D.1587, MEM[(volatile struct SPI *)4096B].CTRL
+	or	a5,a5,a4	# D.1587, D.1587, tmp93
+	sw	a5,16(s0)	# D.1587, MEM[(volatile struct SPI *)4096B].CTRL
 	call	spi_read	#
 	lw	ra,12(sp)	#,
-	lw	a0,0(s0)	# D.1655, MEM[(volatile struct SPI *)4096B].DATA
+	lw	a0,0(s0)	# D.1588, MEM[(volatile struct SPI *)4096B].DATA
 	lw	s0,8(sp)	#,
 	add	sp,sp,16	#,,
 	jr	ra	#
@@ -216,27 +160,42 @@ flash_read_word:
 	.globl	main
 	.type	main, @function
 main:
-	add	sp,sp,-16	#,,
+	add	sp,sp,-32	#,,
 	li	a0,0	#,
-	sw	ra,12(sp)	#,
+	sw	s0,24(sp)	#,
+	sw	s2,16(sp)	#,
+	sw	s3,12(sp)	#,
+	sw	ra,28(sp)	#,
+	sw	s1,20(sp)	#,
 	call	flash_read_word	#
-	mv	a2,a0	#,
-	li	a1,4	#,
-	li	a0,1048576	#,
-	call	flash_read	#
-	li	a5,1048576	# tmp74,
+	mv	s3,a0	# size,
+	li	s0,0	# received,
+	li	s2,1048576	# tmp80,
+.L29:
+	add	s1,s0,s2	# D.1605, received, tmp80
+	bgeu	s0,s3,.L32	#, received, size,
+	add	s0,s0,4	# received, received,
+	mv	a0,s0	#, received
+	call	flash_read_word	#
+	sw	a0,0(s1)	# D.1603, *_14
+	j	.L29	#
+.L32:
  #APP
 # 30 "utils.h" 1
-	jalr x0, a5	# tmp74
+	jalr x0, s1	# D.1605
 nop
 nop
 nop
 
 # 0 "" 2
  #NO_APP
-	lw	ra,12(sp)	#,
+	lw	ra,28(sp)	#,
 	li	a0,1	#,
-	add	sp,sp,16	#,,
+	lw	s0,24(sp)	#,
+	lw	s1,20(sp)	#,
+	lw	s2,16(sp)	#,
+	lw	s3,12(sp)	#,
+	add	sp,sp,32	#,,
 	jr	ra	#
 	.size	main, .-main
 	.ident	"GCC: (GNU) 5.3.0"
